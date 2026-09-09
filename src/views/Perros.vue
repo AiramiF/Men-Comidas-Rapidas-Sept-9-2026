e44118d8e4d02c250b1650a1004c713d88ad8754
FernandaGDz <fer_nanda217@hotmail.com>
2026-09-09 11:43:21 -0500

    Update Perros

--- src/views/Perros.vue
+++ src/views/Perros.vue
@@ -65,7 +65,7 @@
           <q-card class="full-height">
 
             <q-img
-              src="https://images.unsplash.com/photo-1612392061786-6a5a5c8b0e6e?auto=format&fit=crop&w=800&q=80"
+              src="https://images.unsplash.com/photo-1619740455993-9e612b1af08a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGhvdCUyMGRvZ3xlbnwwfHwwfHx8MA%3D%3D"
               height="220px"
             />
 
@@ -137,7 +137,7 @@
           <q-card class="full-height">
 
             <q-img
-              src="https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?auto=format&fit=crop&w=800&q=80"
+              src="https://images.unsplash.com/photo-1613482084286-41f25b486fa2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG90JTIwZG9nfGVufDB8fDB8fHww"
               height="220px"
             />
 
@@ -173,7 +173,7 @@
           <q-card class="full-height">
 
             <q-img
-              src="https://images.unsplash.com/photo-1612392061786-6a5a5c8b0e6e?auto=format&fit=crop&w=800&q=80"
+              src="https://images.unsplash.com/photo-1695089028533-c6e9cf1ee3cb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGhvdCUyMGRvZ3xlbnwwfHwwfHx8MA%3D%3D"
               height="220px"
             />
 
